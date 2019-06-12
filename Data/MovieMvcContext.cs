d3d3c705d4e2360715b3cc088d36ba35c800fe68
edanielbluford <e.daniel.bluford@outlook.com>
2019-06-12 14:24:50 -0400

    First Commit

---
+++ Data/MovieMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MovieMvc.Models
+{
+    public class MovieMvcContext : DbContext
+    {
+        public MovieMvcContext (DbContextOptions<MovieMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
